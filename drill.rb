699bf65d83d6b42d293d14bff32b2473043cae5e
no215tyler <no.215.tyler@gmail.com>
2024-01-03 20:51:38 +0900

    drill02

--- drill.rb
+++ drill.rb
@@ -1,7 +1,18 @@
 # Rubyドリル01
-hash = {one: 1, two: 2, three: 3}
+# hash = {one: 1, two: 2, three: 3}
 
-puts hash.keys
-puts hash.values
+# puts hash.keys
+# puts hash.values
 # -------------------------
 # Rubyドリル02
+user_data = [
+ {user: {profile: {name: 'George'}}},
+ {user: {profile: {name: 'Alice'}}},
+ {user: {profile: {name: 'Taro'}}},
+]
+
+user_data.each do |user|
+  puts user[:user][:profile][:name]
+end
+# -------------------------
+# Rubyドリル03
