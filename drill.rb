18e55e3869d293ddb9ade8ee282ecb7d0221f7ed
no215tyler <no.215.tyler@gmail.com>
2024-01-03 21:36:48 +0900

    drill03

--- drill.rb
+++ drill.rb
@@ -5,14 +5,18 @@
 # puts hash.values
 # -------------------------
 # Rubyドリル02
-user_data = [
- {user: {profile: {name: 'George'}}},
- {user: {profile: {name: 'Alice'}}},
- {user: {profile: {name: 'Taro'}}},
-]
+# user_data = [
+#  {user: {profile: {name: 'George'}}},
+#  {user: {profile: {name: 'Alice'}}},
+#  {user: {profile: {name: 'Taro'}}},
+# ]
 
-user_data.each do |user|
-  puts user[:user][:profile][:name]
-end
+# user_data.each do |user|
+#   puts user[:user][:profile][:name]
+# end
 # -------------------------
 # Rubyドリル03
+scores = {japanese_score: 80, english_score: 50, math_score: 70}
+average_score = scores.values.reduce(:+) / scores.length
+
+puts "3教科の平均点は#{average_score}点です。"
