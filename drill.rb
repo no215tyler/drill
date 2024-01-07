2f67031dc718dbfcc3faf7091c729bdcf3a6a382
no215tyler <no.215.tyler@gmail.com>
2024-01-07 17:46:53 +0900

    drill16

--- drill.rb
+++ drill.rb
@@ -151,12 +151,43 @@
 # in1to10(11,true)
 # -------------------------
 # Rubyドリル15
-def output(input)
-  input.times do
-    puts "Hello!"
-  end
-end
+# def output(input)
+#   input.times do
+#     puts "Hello!"
+#   end
+# end
 
-puts "何回表示させますか？"
-input = gets.to_i
-output(input)
+# puts "何回表示させますか？"
+# input = gets.to_i
+# output(input)
+# -------------------------
+# Rubyドリル16
+class Fruit
+  def self.flesh
+    puts "採れたて新鮮な果実です"
+  end
+ 
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+ 
+  def introduce
+    puts "#{@name}は#{@price}円です"
+  end
+ end
+ 
+ 
+ # 3つのインスタンスを生成してください
+apple = Fruit.new("リンゴ", 120)
+orange = Fruit.new("オレンジ", 200)
+strawberry = Fruit.new("イチゴ", 60)
+ 
+ # クラスメソッドを呼び出し、「採れたて新鮮な果実です」と表示してください
+ Fruit.flesh
+ # インスタンス毎にインスタンスメソッドを呼び出し、「【名前】は【価格】円です」と表示してください
+ apple.introduce
+ orange.introduce
+ strawberry.introduce
+ # -------------------------
+ 
