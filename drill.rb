1b2c6c8afe1249070410bc9e035745d7438d8cd1
no215tyler <no.215.tyler@gmail.com>
2024-02-19 17:32:36 +0900

    drill51

--- drill.rb
+++ drill.rb
@@ -689,17 +689,62 @@
 # include_cat_and_dog?('flogdog')
 # -------------------------
 # Rubyドリル50
-while true
-  puts "[0]:カロリーを表示する\n[1]:終了する"
-  choices = gets.chomp
-  if /\A[01]\z/.match?(choices)
-    case choices
-    when '0'
-      puts '500kcal'
-    when '1'
-      exit
-    end
+# while true
+#   puts "[0]:カロリーを表示する\n[1]:終了する"
+#   choices = gets.chomp
+#   if /\A[01]\z/.match?(choices)
+#     case choices
+#     when '0'
+#       puts '500kcal'
+#     when '1'
+#       exit
+#     end
+#   else
+#     puts "[0]か[1]を選択してください"
+#   end
+# end
+# -------------------------
+# Rubyドリル51
+def register_book
+  # 本の著者、タイトル、価格をユーザーに入力させ、保存する
+  puts '著者を入力してください'
+  author = gets.chomp
+  puts 'タイトルを入力してください'
+  title = gets.chomp
+  puts '価格を入力してください'
+  price = gets.chomp
+  books = { author: author, title: title, price: price }
+end
+
+def show_books(books)
+  puts "見たい番号を入力してください"
+  books.each_with_index do |book, index|
+    puts "【#{index + 1}】#{book[:title]}"
+  end
+  show_detail(books[gets.to_i - 1])
+end
+
+def show_detail(book)
+  # 選択された本の詳細な情報（著者、タイトル、価格）を出力する
+  puts "著者: #{book[:author]}"
+  puts "タイトル: #{book[:title]}"
+  puts "価格: #{book[:price]}円"
+end
+
+books = []
+while true do
+  puts "番号を入力してください"
+  puts "0: 本を登録する"
+  puts "1: 本の一覧を見る"
+  puts "2: 終了する"
+  case gets.to_i
+  when 0
+    books << register_book
+  when 1
+    show_books(books)
+  when 2
+    exit
   else
-    puts "[0]か[1]を選択してください"
+    puts '無効な値です'
   end
 end
