03d22ce39778fc20c443a35118a42ceea8f0ca52
no215tyler <no.215.tyler@gmail.com>
2024-02-23 16:08:06 +0900

    drill66

--- drill.rb
+++ drill.rb
@@ -1016,45 +1016,111 @@
 # puts "#{week_array[now.wday]}曜日です"
 # -------------------------
 # Rubyドリル65
-def janken
-  puts "[0]:グー\n[1]:チョキ\n[2]:パー"
+# def janken
+#   puts "[0]:グー\n[1]:チョキ\n[2]:パー"
 
-  # ターミナルからじゃんけんの手を入力
-  player_hand = gets.to_i
+#   # ターミナルからじゃんけんの手を入力
+#   player_hand = gets.to_i
 
-  # 乱数でプログラム側の手を決定
-  program_hand = rand(3)
+#   # 乱数でプログラム側の手を決定
+#   program_hand = rand(3)
 
-  jankens = %w(グー チョキ パー)
+#   jankens = %w(グー チョキ パー)
 
-  puts "あなたの手:#{jankens[player_hand]}, わたしの手:#{jankens[program_hand]}"
+#   puts "あなたの手:#{jankens[player_hand]}, わたしの手:#{jankens[program_hand]}"
 
-  # あいこの判定はplayer_handとprogram_handの値が等しいとき
-  if  player_hand == program_hand
-    puts "あいこで"
-    # 返り値を返す
-    return true
+#   # あいこの判定はplayer_handとprogram_handの値が等しいとき
+#   if  player_hand == program_hand
+#     puts "あいこで"
+#     # 返り値を返す
+#     return true
 
-  # じゃんけんに勝つパターンを全て並べる
-  elsif player_hand == 0 && program_hand == 1 || 
-        player_hand == 1 && program_hand == 2 || 
-        player_hand == 2 && program_hand == 0
-    puts "あなたの勝ちです"
-  # 返り値を返す
-    return false
+#   # じゃんけんに勝つパターンを全て並べる
+#   elsif player_hand == 0 && program_hand == 1 || 
+#         player_hand == 1 && program_hand == 2 || 
+#         player_hand == 2 && program_hand == 0
+#     puts "あなたの勝ちです"
+#   # 返り値を返す
+#     return false
 
-  else
-    puts "あなたの負けです"
-    # 返り値を返す
-    return false
-  end
+#   else
+#     puts "あなたの負けです"
+#     # 返り値を返す
+#     return false
+#   end
+# end
+
+# next_game = true
+
+# puts "最初はグー、じゃんけん..."
+
+# while next_game do
+#   # jankenメソッドの返り値をnext_gameに代入
+#   next_game = janken
+# end
+# -------------------------
+# Rubyドリル66
+def registration_student
+  student = {}
+  puts '生徒名を入力してください'
+    student[:name] = gets.chomp
+  puts '生徒の年齢を入力してください'
+    student[:age] = gets.to_i
+  puts "国語の得点は？"
+    student[:japanese] = get_valid_score
+  puts "数学の得点は？"
+    student[:math] = get_valid_score
+  puts "英語の得点は？"
+    student[:english] = get_valid_score
+  return student
 end
 
-next_game = true
+def show_student_name(data)
+  puts '見たい生徒の番号を入力してください'
+  data.each_with_index do |data, index|
+    puts "  【#{index + 1}】#{data[:name]}"
+  end
+  choose_num = get_valid_input(data) - 1
+  puts "名前:#{data[choose_num][:name]}"
+  puts "年齢:#{data[choose_num][:age]}"
+  puts "国語:#{data[choose_num][:japanese]}"
+  puts "数学:#{data[choose_num][:math]}"
+  puts "英語:#{data[choose_num][:english]}"
+end
 
-puts "最初はグー、じゃんけん..."
+def get_valid_score
+  score = gets.to_i
+  while score < 0 || score > 100
+    puts "0 〜 100の数値を入力してください"
+    score = gets.to_i
+  end
+  return score
+end
 
-while next_game do
-  # jankenメソッドの返り値をnext_gameに代入
-  next_game = janken
+def get_valid_input(data)
+  input = gets.to_i
+  while input < data.length || input > data.length
+    puts "無効な値です"
+    input = gets.to_i
+  end
+  return input
+end
+
+students = []
+
+while true
+  puts '行いたい項目を選択してください'
+  puts '[1]点数を登録する'
+  puts '[2]点数を確認する'
+  puts '[3]終了する'
+  input = gets.to_i
+  if input == 1
+    students << registration_student
+  elsif input == 2
+    show_student_name(students)
+  elsif input == 3
+    exit
+  else
+    puts '無効な値です'
+  end
 end
