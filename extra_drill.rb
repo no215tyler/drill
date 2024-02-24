27c59331a657ec87c99e9a5d8793119e8d5c4e1c
no215tyler <no.215.tyler@gmail.com>
2024-02-24 23:14:50 +0900

    drill73

--- extra_drill.rb
+++ extra_drill.rb
@@ -1,68 +1,90 @@
 # Rubyドリル71 & Rubyドリル72
-def register_item(registered_item)
-  items = {}
-  puts "商品名を入力してください："
-    items[:name] = gets.chomp
-  puts "販売価格を入力してください："
-    items[:price] = gets.to_i
-  puts "仕入れ値を入力してください："
-    items[:purchase] = gets.to_i
-    items[:profit] = items[:price] - items[:purchase]
-    items[:profit_rate] = (items[:profit].to_f/ items[:price].to_f * 100).round(2)
-    line = "---------------------------"
+# def register_item(registered_item)
+#   items = {}
+#   puts "商品名を入力してください："
+#     items[:name] = gets.chomp
+#   puts "販売価格を入力してください："
+#     items[:price] = gets.to_i
+#   puts "仕入れ値を入力してください："
+#     items[:purchase] = gets.to_i
+#     items[:profit] = items[:price] - items[:purchase]
+#     items[:profit_rate] = (items[:profit].to_f/ items[:price].to_f * 100).round(2)
+#     line = "---------------------------"
 
-  puts "商品名 : #{items[:name]}\n#{line}"
-  puts "販売価格 : #{items[:price]}円\n#{line}"
-  puts "仕入れ値 : #{items[:purchase]}円\n#{line}"
-  registered_item << items
-end
+#   puts "商品名 : #{items[:name]}\n#{line}"
+#   puts "販売価格 : #{items[:price]}円\n#{line}"
+#   puts "仕入れ値 : #{items[:purchase]}円\n#{line}"
+#   registered_item << items
+# end
 
-def check_items(registered_item)
-  total_sales = 0
-  total_profit = 0
-  line = "---------------------------"
-  puts "【商品一覧】\n#{line}"
+# def check_items(registered_item)
+#   total_sales = 0
+#   total_profit = 0
+#   line = "---------------------------"
+#   puts "【商品一覧】\n#{line}"
 
-  registered_item.each do |item|
-    puts "商品名：#{item[:name]}"
-    puts "販売価格：#{item[:price]}円"
-    puts "仕入れ値：#{item[:purchase]}円\n#{line}"
-    puts "利益 : #{item[:profit]}円\n#{line}"
-    puts "利益率 : #{item[:profit_rate]}%\n#{line}"
-    total_sales += item[:price]
-    total_profit += item[:profit]
+#   registered_item.each do |item|
+#     puts "商品名：#{item[:name]}"
+#     puts "販売価格：#{item[:price]}円"
+#     puts "仕入れ値：#{item[:purchase]}円\n#{line}"
+#     puts "利益 : #{item[:profit]}円\n#{line}"
+#     puts "利益率 : #{item[:profit_rate]}%\n#{line}"
+#     total_sales += item[:price]
+#     total_profit += item[:profit]
+#   end
+#   puts "総売上 :#{total_sales}円\n#{line}"
+#   puts "総利益 :#{total_profit}円\n#{line}"
+#   average_profit_rate = (total_profit.to_f / total_sales.to_f * 100 / registered_item.length).round(2)
+#   puts "平均利益率 :#{average_profit_rate}%\n#{line}"
+# end
+
+# def end_program
+#   exit
+# end
+
+# def exception
+#   puts "入力された値は無効な値です"
+# end
+
+# item_list = []
+
+# while true do
+#   puts "商品数: #{item_list.length}"
+#   puts "[1]商品を登録する"
+#   puts "[2]商品の一覧を確認する" unless item_list.empty?
+#   puts "[3]アプリを終了する"
+
+#   input = gets.to_i
+
+#   if input == 1
+#     register_item(item_list)
+#   elsif input == 2
+#     check_items(item_list)
+#   elsif input == 3
+#     end_program
+#   else
+#     exception
+#   end
+# end
+# -------------------------
+# Rubyドリル73
+def bubble_sort(data)
+  # 配列の数を数える処理を記述
+  length = data.length
+
+  # for文を2つ使用する
+  for i in 0...length
+    for j in 0...length
+      # 先頭から隣の数同士の大きさを比べる
+      if data[i] < data[j]
+        # 先頭側の要素の方が大きい場合は、配列の位置を隣同士で交換する
+        data[i], data[j] = data[j], data[i]
+      end
+    end
   end
-  puts "総売上 :#{total_sales}円\n#{line}"
-  puts "総利益 :#{total_profit}円\n#{line}"
-  average_profit_rate = (total_profit.to_f / total_sales.to_f * 100 / registered_item.length).round(2)
-  puts "平均利益率 :#{average_profit_rate}%\n#{line}"
 end
 
-def end_program
-  exit
-end
-
-def exception
-  puts "入力された値は無効な値です"
-end
-
-item_list = []
-
-while true do
-  puts "商品数: #{item_list.length}"
-  puts "[1]商品を登録する"
-  puts "[2]商品の一覧を確認する" unless item_list.empty?
-  puts "[3]アプリを終了する"
-
-  input = gets.to_i
-
-  if input == 1
-    register_item(item_list)
-  elsif input == 2
-    check_items(item_list)
-  elsif input == 3
-    end_program
-  else
-    exception
-  end
-end
+# 呼び出し例
+number = [1,23,4,6,12,45,79]
+bubble_sort(number)
+puts number
