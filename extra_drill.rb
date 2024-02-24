5eae1492e4e9b8e8763bd74011d74e60d829bc21
no215tyler <no.215.tyler@gmail.com>
2024-02-24 23:29:28 +0900

    drill74

--- extra_drill.rb
+++ extra_drill.rb
@@ -68,17 +68,36 @@
 # end
 # -------------------------
 # Rubyドリル73
+# def bubble_sort(data)
+#   # 配列の数を数える処理を記述
+#   length = data.length
+
+#   # for文を2つ使用する
+#   for i in 0...length
+#     for j in 0...length
+#       # 先頭から隣の数同士の大きさを比べる
+#       if data[i] < data[j]
+#         # 先頭側の要素の方が大きい場合は、配列の位置を隣同士で交換する
+#         data[i], data[j] = data[j], data[i]
+#       end
+#     end
+#   end
+# end
+
+# # 呼び出し例
+# number = [1,23,4,6,12,45,79]
+# bubble_sort(number)
+# puts number
+# -------------------------
+# Rubyドリル74
 def bubble_sort(data)
   # 配列の数を数える処理を記述
   length = data.length
 
-  # for文を2つ使用する
   for i in 0...length
-    for j in 0...length
-      # 先頭から隣の数同士の大きさを比べる
-      if data[i] < data[j]
-        # 先頭側の要素の方が大きい場合は、配列の位置を隣同士で交換する
-        data[i], data[j] = data[j], data[i]
+    for j in 1...length - i
+      if (data[j - 1]) < (data[j])
+        data[j - 1], data[j] = data[j], data[j - 1]
       end
     end
   end
